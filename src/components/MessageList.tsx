65c5559a6ab4987624f8608228ad3f8f88281419
mariana castro <marianacastrorc@gmail.com>
2025-09-02 17:09:25 -0300

    updating styles

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -16,7 +16,7 @@ export const MessageList = ({
   isMobile,
 }: MessageListProps) => {
   return (
-    <div className={`flex flex-col h-[100vh] w-full bg-[#212020]`}>
+    <div className={`flex flex-col h-[100dvh] w-full bg-[#212020]`}>
       {!isMobile && currentChatTitle && (
         <div className="sticky top-0 z-10 bg-[#212020] w-full border-b border-[#303133] p-3 pt-1">
           <h1 className="text-base font-medium text-center text-white break-words line-clamp-2">
@@ -25,7 +25,7 @@ export const MessageList = ({
         </div>
       )}
 
-      <div className="flex items-start w-full justify-center mt-4 max-h-[80vh] chat-scroll-container overflow-y-auto">
+      <div className="mt-4 flex items-start w-full justify-center mb-28 chat-scroll-container overflow-y-auto">
         <div className="max-w-4xl pb-4">
           {messages.map((message) => (
             <MessageItem key={message.id} message={message} />
