6a76da14fcc877e664cf7de3cffb413541792595
mariana castro <marianacastrorc@gmail.com>
2025-09-05 17:00:37 -0300

    refactoring styles

--- src/components/MobileHeader/partials/SidebarHeader.tsx
+++ src/components/MobileHeader/partials/SidebarHeader.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export const SidebarHeader = ({ onClose }: { onClose: () => void }) => (
   <div className="flex items-center justify-between p-4 pb-2">
-    <Image width={130} height={130} alt="Logo" src="/logo-full.svg" />
+    <Image width={150} height={150} alt="Logo" src="/logo-full.svg" />
     <button
       onClick={onClose}
       aria-label="Close menu"
